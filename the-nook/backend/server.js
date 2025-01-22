5dfc8785cd4912b4878d59394465a78639de1c03
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2025-01-22 19:39:33 +1100

    Add comments/documentation

--- the-nook/backend/server.js
+++ the-nook/backend/server.js
@@ -2,22 +2,40 @@ import mongoose from 'mongoose';
 import express from 'express';
 import dotenv from 'dotenv';
 
-// Configure between commonJS (default) and ES6 module in package.json
-// const mongoose = require('mongoose');
-// const express = require('express');
-// const dotenv = require('dotenv');
 
+/**
+ * Loads the environment variables in .env file
+ * (e.g. MONGO_URI, PORT) into the process.env object.
+ */
 dotenv.config();
+
+/**
+ * Initializing an Express app. This is an app instance.
+ * It handles HTTP requests, routes, middleware, and responses.
+ */
 const app = express();
 
-// Enable JSON parsing
+/**
+ * Enables JSON parsing.
+ * This middleware tells Express to parse incoming requests with JSON payloads.
+ * It makes the data available in req.body.
+ * 
+ * This allows the app to handle JSON requests correctly (e.g. creating, updating resources).
+ */
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB is connected!'))
-    .catch(err => console.error('MongoDB connection error:', err));
+/**
+ * Connecting to MongoDB.
+ */
+mongoose.connect(process.env.MONGO_URI) // connects using connection string stored in process.env.MONGO_URI
+    .then(() => console.log('MongoDB is connected!')) // post successful connection -> logs "successful connection!"
+    .catch(err => console.error('MongoDB connection error:', err)); // catches and logs errors during connection process
 
-
+/**
+ * process.env.PORT dynamically retrieves the port from the .env file (useful for deployment).
+ * Default PORT is 5000 is none is specified.
+ * 
+ * app.listen() starts the server, begins listening for incoming HTTP requests.
+ */
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
