7be94e2455925da7659a9f3f1710267f9cff2b99
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2025-01-22 19:20:12 +1100

    Update import statements to match ES6 syntax

--- the-nook/backend/server.js
+++ the-nook/backend/server.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const dotenv = require('dotenv');
+import mongoose from 'mongoose';
+import express from 'express';
+import dotenv from 'dotenv';
 
-// ES6 syntax leads to node error???
-// import mongoose from 'mongoose';
-// import express from 'express';
-// import dotenv from 'dotenv';
+// Configure between commonJS (default) and ES6 module in package.json
+// const mongoose = require('mongoose');
+// const express = require('express');
+// const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
 
+// Enable JSON parsing
 app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected!'))
+    .then(() => console.log('MongoDB is connected!'))
     .catch(err => console.error('MongoDB connection error:', err));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
