b20ad33f3c7be5f07f58373e7ffe33c24b7241c3
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2025-01-22 21:25:33 +1100

    Add Routerlink

--- the-nook/src/app/app.component.ts
+++ the-nook/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'the-nook';
+  title = 'The Book Nook';
 }
