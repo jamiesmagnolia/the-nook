0ec9f6941dbdefb4cef9c9b2c387fb88378be3e6
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2025-01-23 01:58:32 +1100

    Update routes

--- the-nook/src/app/app.routes.ts
+++ the-nook/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
-        title: 'The Nook | Home'
+        title: 'The Book Nook | Home',
+        component: HomeComponent,
     },
-    {},
+
+    {
+        path: 'user-dashboard',
+        title: 'The Book Nook | Dashboard',
+        component: DashboardComponent,
+    }
 ];
