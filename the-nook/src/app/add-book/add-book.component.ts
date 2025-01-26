6b8eb7b60d0b8991df082b082ccf9b7915a7c037
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2025-01-27 04:40:33 +1100

    Add chips for genre selections

--- the-nook/src/app/add-book/add-book.component.ts
+++ the-nook/src/app/add-book/add-book.component.ts
@@ -7,7 +7,17 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatSlider, MatSliderModule} from '@angular/material/slider';
 import {MatChipEditedEvent, MatChipInputEvent, MatChipsModule} from '@angular/material/chips';
 import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
 
+
+// export interface Fruit {
+//   name: string;
+// }
+
+export interface Genre {
+  name: string;
+}
 
 @Component({
   selector: 'app-add-book',
@@ -19,6 +29,7 @@ import {LiveAnnouncer} from '@angular/cdk/a11y';
     MatSelectModule,
     MatSliderModule,
     MatChipsModule,
+    MatIconModule
   ],
   templateUrl: './add-book.component.html',
   styleUrl: './add-book.component.scss',
@@ -33,5 +44,68 @@ export class AddBookComponent {
   bookStatus = new FormControl('');
 
   onInput() {}
+
+  readonly addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+
+//   [
+//     'Fiction', 
+//     'Non-Fiction', 
+//     'Sci-Fi', 
+//     'Fantasy', 
+//     'Romance', 
+//     'Mystery', 
+//     'Biography', 
+//     'History', 
+//     'Other'
+// ],
+
+  readonly genres = signal<Genre[]>([{name: 'Fiction'}, {name: 'Fantasy'}, {name: 'Adventure'}]);
+  readonly announcer = inject(LiveAnnouncer);
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our genre
+    if (value) {
+      this.genres.update(genres => [...genres, {name: value}]);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  remove(genre: Genre): void {
+    this.genres.update(genres => {
+      const index = genres.indexOf(genre);
+      if (index < 0) {
+        return genres;
+      }
+
+      genres.splice(index, 1);
+      this.announcer.announce(`Removed ${genre.name}`);
+      return [...genres];
+    });
+  }
+
+  edit(genre: Genre, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    // Remove fruit if it no longer has a name
+    if (!value) {
+      this.remove(genre);
+      return;
+    }
+
+    // Edit existing fruit
+    this.genres.update(genres => {
+      const index = genres.indexOf(genre);
+      if (index >= 0) {
+        genres[index].name = value;
+        return [...genres];
+      }
+      return genres;
+    });
+  }
 
 }
