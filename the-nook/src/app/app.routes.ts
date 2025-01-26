898fa9938f575c7d3cc6ecbcd610fc7721750fbc
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2025-01-27 04:31:01 +1100

    Add add-book route

--- the-nook/src/app/app.routes.ts
+++ the-nook/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { AddBookComponent } from './add-book/add-book.component';
 
 export const routes: Routes = [
     {
@@ -13,5 +14,11 @@ export const routes: Routes = [
         path: 'user-dashboard',
         title: 'The Book Nook | Dashboard',
         component: DashboardComponent,
+    },
+
+    {
+        path: 'add-book',
+        title: 'The Book Nook | Add Book',
+        component: AddBookComponent,
     }
 ];
