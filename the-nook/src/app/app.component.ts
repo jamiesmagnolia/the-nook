bb57242e21f5e0776cece4468971aff05de509a5
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2025-02-11 00:39:58 +0700

    Add Header and Update imports

--- the-nook/src/app/app.component.ts
+++ the-nook/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HeaderComponent } from "./layouts/header/header.component";
 
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterLink, 
-    RouterOutlet, 
-    DashboardComponent
-  ],
+    RouterLink,
+    RouterOutlet,
+    HeaderComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
